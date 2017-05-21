c22a337e413df977454cc6b0e39df7550c68a4e2
nobody at office <none@office.com>
2017-05-21 10:37:00 +0800

    no comment

--- usb_c/xusb.c
+++ usb_c/xusb.c
@@ -19,13 +19,25 @@
 
 // 变量定义
 DEVICE_STATUS    gDeviceStatus;
-
+EP0_COMMAND      gEp0Command;
 
 void power_init(void)
 {
   // default value
   // 设置USB设备是否自供电
   //REG0CN = 0;
+}
+void port_init(void)
+{
+  // set led
+  //P3MDIN = 1;
+  // set push-pull
+  P3MDOUT  = 1;
+  //P1MDIN = 0b00000111;
+  P1MDOUT  = 0b00000111;
+  // All led off
+  LED1     = 0;
+  P1       = 0b00000111;
 }
 void init(void){
   EA=0;
@@ -38,7 +50,7 @@ void init(void){
   OSCICN = 0b10000011;
   while(!(OSCICN & 0b01000000));
   XBR0 = 0;
-  XBR1 =0x40;
+  XBR1 =0x40; // enable cross bar
 }
 void init_usb(void){
   // clock setting
@@ -76,14 +88,19 @@ void main(void){
 }
 void usb_reset(void);
 void usb_endpoint0(void);
+void fifo_read(uint8_t, uint16_t, uint8_t *);
+void fifo_write(uint8_t, uint16_t, uint8_t *);
 // 对于中断， __using 好像没用
 void usb_int(void)  __interrupt(8) __using(2) __naked{
   uint8_t cmint;
   uint8_t in1int;
   uint8_t out1int;
-  // 主程序没有任何业务，因此不需要保存环境
-  //__asm__ ("push psw");
-  //__asm__ ("push acc");
+  __asm__ ("push psw");
+  __asm__ ("setb rs1");
+  __asm__ ("clr  rs0");
+  __asm__ ("push acc");
+  __asm__ ("push dpl");
+  __asm__ ("push dph");
 
   UREAD_BYTE(CMINT,cmint);
   UREAD_BYTE(IN1INT,in1int);
@@ -102,10 +119,11 @@ void usb_int(void)  __interrupt(8) __using(2) __naked{
   if(out1int & rbOUT2){
 	// endpoint2 out
   }
-
-  //__asm__ ("pop acc");
-  //__asm__ ("pop psw");
-  __asm__ ("reti \n"); //for naked function
+  __asm__ ("push dph");
+  __asm__ ("push dpl");
+  __asm__ ("pop acc");
+  __asm__ ("pop psw");
+  __asm__ ("reti"); //for naked function
 }
 
 
@@ -124,4 +142,96 @@ void usb_reset(void)
 }
 void usb_endpoint0(void)
 {
+   uint8_t bCsr1, uTxBytes;
+
+   UWRITE_BYTE(INDEX, 0);                 // Target ep0
+   UREAD_BYTE(E0CSR, bCsr1);
+   if(bCsr1 & rbSUEND){
+	 UWRITE_BYTE(E0CSR,rbSSUEND);
+   }
+   if (bCsr1 & rbSTSTL){                  // If last state requested a stall
+                                          // Clear Sent Stall bit (STSTL)
+      UWRITE_BYTE(E0CSR, 0);
+   }
+
+   // Handle incoming packet
+   if (bCsr1 & rbOPRDY){
+	 fifo_read(0,8,(uint8_t *)&gEp0Command);
+   }
+}
+
+
+//
+// Return Value : None
+// Parameters   : 
+// 1) BYTE bEp
+// 2) UINT uNumBytes
+// 3) BYTE * pData
+//
+// Read from the selected endpoint FIFO
+//
+//-----------------------------------------------------------------------------
+void fifo_read(uint8_t bEp, uint16_t uNumBytes, uint8_t * pData)
+{
+   uint8_t TargetReg;
+   uint16_t i;
+
+   // If >0 bytes requested,
+   if (uNumBytes) {
+      TargetReg = FIFO_EP0 + bEp;         // Find address for target
+                                          // endpoint FIFO
+
+      USB0ADR = (TargetReg & 0x3F);       // Set address (mask out bits7-6)
+      USB0ADR |= 0xC0;                    // Set auto-read and initiate
+                                          // first read
+
+      // Unload <NumBytes> from the selected FIFO
+      for(i=0;i<uNumBytes-1;i++)
+      {
+         while(USB0ADR & 0x80);           // Wait for BUSY->'0' (data ready)
+         pData[i] = USB0DAT;              // Copy data byte
+      }
+
+
+      while(USB0ADR & 0x80);              // Wait for BUSY->'0' (data ready)
+      pData[i] = USB0DAT;                 // Copy data byte
+      USB0ADR = 0;                        // Clear auto-read
+   }
+}
+
+//-----------------------------------------------------------------------------
+// FIFOWrite
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : 
+// 1) BYTE bEp
+// 2) UINT uNumBytes
+// 3) BYTE * pData
+//
+// Write to the selected endpoint FIFO
+//
+//-----------------------------------------------------------------------------
+void fifo_write (uint8_t bEp, uint16_t uNumBytes, uint8_t * pData)
+{
+   uint8_t TargetReg;
+   uint16_t i;
+
+   // If >0 bytes requested,
+   if (uNumBytes)
+   {
+      TargetReg = FIFO_EP0 + bEp;         // Find address for target
+                                          // endpoint FIFO
+
+      while(USB0ADR & 0x80);              // Wait for BUSY->'0'
+                                          // (register available)
+      USB0ADR = (TargetReg & 0x3F);       // Set address (mask out bits7-6)
+
+      // Write <NumBytes> to the selected FIFO
+      for(i=0;i<uNumBytes;i++)
+      {
+         USB0DAT = pData[i];
+         while(USB0ADR & 0x80);           // Wait for BUSY->'0' (data ready)
+      }
+   }
 }
